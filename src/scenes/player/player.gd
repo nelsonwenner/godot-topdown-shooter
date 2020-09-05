29fd174bb315528ef39bdc8132d492f3e4f5b584
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-05 16:11:19 -0300

    feat: create player, wapons scenes. added get wapons with player

--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -2,19 +2,20 @@ extends KinematicBody2D
 
 var SPEED = 100
 
+func _process(_delta):
+	look_at(get_global_mouse_position())
+
 func _physics_process(_delta):
-	var look_to = get_global_mouse_position() - global_position
-	rotation = atan2(look_to.x, look_to.y * -1)
+	var motion = Vector2(0,0)
 	
-	var direction = Vector2(0,0)
-	
-	if Input.is_action_pressed("player_down"):
-		direction.y += SPEED
-	elif Input.is_action_pressed("player_up"):
-		direction.y -= SPEED
+	if Input.is_action_pressed("player_up"):
+		motion.y -= SPEED
+	elif Input.is_action_pressed("player_down"):
+		motion.y += SPEED
 	if Input.is_action_pressed("player_right"):
-		direction.x += SPEED
+		motion.x += SPEED
 	elif Input.is_action_pressed("player_left"):
-		direction.x -= SPEED
-		
-	move_and_slide(direction)
+		motion.x -= SPEED
+			
+	motion = motion.normalized()
+	motion = move_and_slide(motion * SPEED)
