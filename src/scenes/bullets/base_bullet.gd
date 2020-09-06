80f1c33745b517f0e9abf3470fd40c362494d855
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-06 13:33:39 -0300

    feat: added _ini in weapon

--- src/scenes/bullets/base_bullet.gd
+++ src/scenes/bullets/base_bullet.gd
@@ -1,24 +1,26 @@
 extends KinematicBody2D
 
-var max_distance = -1
 var origin = Vector2(0,0)
 var dir = Vector2(0,0)
+var max_distance = -1
+var KNOCKBACK = 0
 var SPEED = 800
 var DMG = 100
-var KNOCKBACK = 0
 
-func _ini(direction,velocity,pos,distance,damage,knockb,col,size):
+func _ini(direction,velocity,pos,rota,distance,damage,knockb,col,size):
 	scale = Vector2(size,size)
 	$MeshInstance2D.modulate = col
 	DMG = damage
 	origin = pos
 	position = pos
+	rotation = rota
 	dir = direction
 	SPEED = velocity
 	max_distance = distance
 	KNOCKBACK = knockb
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	move_and_slide(dir * SPEED)
 	if (origin - position).length() > max_distance:
 		queue_free()
+		print("Bullet Destroyed!!")
--- src/scenes/weapons/weapon.gd
+++ src/scenes/weapons/weapon.gd
@@ -1,14 +1,59 @@
 extends Area2D
 
+onready var bullet = preload("res://src/scenes/bullets/base_bullet.tscn")
+export var bullet_color:Color
+
+var BULLET_SIZE = 1
+var BULLETS = 1
+var BULLET_SPEED = 500
+var SHOOT_RAGE = 700
+var KNOCKBACK = 0
+var DAMAGE = 10
+var STRAY = 0.1
+
+var SHOOT_WEAPON_TIME = 0.1
+var RELOAD_WEAPON_TIME = 2.5
+
+var MAX_SLOT = 36
+var SLOT = -1
+
+var WEAPON_TYPE = "UZI"
+
+var can_shoot = true 
+
 var can_pick: bool
 var picked = false
 var player_hand
 var player
 
+
+func _ini(type,shoot_weapon_time,reload_weapon_time,bullet_quantity,bullet_speed,bullet_size,shoot_range,knockback,max_slot,damage):
+	SHOOT_WEAPON_TIME = shoot_weapon_time
+	RELOAD_WEAPON_TIME = reload_weapon_time
+	BULLETS = bullet_quantity
+	BULLET_SPEED = bullet_speed
+	BULLET_SIZE = bullet_size
+	SHOOT_RAGE = shoot_range
+	KNOCKBACK = knockback
+	MAX_SLOT = max_slot
+	DAMAGE = damage
+	WEAPON_TYPE = type
+	
+
+func _ready():
+	$shoot_weapon_time.wait_time = SHOOT_WEAPON_TIME
+	$reload_weapon_time.wait_time = RELOAD_WEAPON_TIME
+	SLOT = MAX_SLOT
+
+
 func _process(_delta):
 	if can_pick:
 		if Input.is_action_just_pressed("pickup") && !picked:
-			pick(player, "player_position_weapon")
+			_pick(player, "player_position_weapon")
+			
+		if Input.is_action_pressed("shoot") and can_shoot:
+			_shoot()
+
 
 func _physics_process(_delta):
 	if picked:
@@ -17,8 +62,9 @@ func _physics_process(_delta):
 		scale.y = 0.5
 		position = player_hand.get_global_position() + Vector2(0, 0).rotated(rotation)
 		rotation = player_hand.get_global_rotation()
+		_reload()
 		
-		
+
 func _on_weapon_body_entered(_body):
 	var bodies = $".".get_overlapping_bodies()
 	for body in bodies:
@@ -27,7 +73,49 @@ func _on_weapon_body_entered(_body):
 			can_pick = true
 
 
-func pick(picker_current, player_position_hand):
+func _shoot():
+	if SLOT > 0:
+		for i in BULLETS:
+			var bullet_instance = bullet.instance()
+			var weapon_position = $weapon_position.get_global_position() + Vector2(0, 0).rotated(rotation)
+			var weapon_rotation = $weapon_position.get_global_rotation() + deg2rad(90)
+			
+			var target = (get_global_mouse_position() - weapon_position).normalized()
+			
+			if STRAY > 0:
+				target = target.rotated(rand_range(STRAY*-1,STRAY))
+			
+			bullet_instance._ini(
+				target,rand_range(BULLET_SPEED*0.9, BULLET_SPEED*1.1),
+				weapon_position, weapon_rotation,
+				SHOOT_RAGE, DAMAGE, KNOCKBACK,
+				bullet_color, BULLET_SIZE
+			)
+			$bullet_container.add_child(bullet_instance)
+			SLOT -= 1
+			$CanvasLayer/munition.text = str(SLOT) + "/" + str(MAX_SLOT)
+		can_shoot = false
+		$shoot_weapon_time.start()
+		
+		
+func _pick(picker_current, player_position_hand):
 	player_hand = picker_current.get_node(player_position_hand) # reference player_hand
 	position = picker_current.get_global_position()
 	picked = true
+
+
+func _reload():
+	if Input.is_action_just_pressed("reload") && SLOT <= 0:
+		$reload_weapon_time.start()
+		print("reload...")
+	
+	
+func _on_shoot_weapon_time_timeout():
+	can_shoot = true
+
+
+func _on_reload_weapon_time_timeout():
+	SLOT = MAX_SLOT
+	$CanvasLayer/munition.text = str(SLOT) + "/" + str(MAX_SLOT)
+	$reload_weapon_time.stop()
+	can_shoot = false
